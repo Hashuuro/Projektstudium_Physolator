c623861ec55c7024c491f15a2d409f5c179c671f
Hashuuro <Lukas_Walther@gmx.de>
2018-05-28 22:35:34 +0200

    Bewegung  implementiert
    - Räder laufen
    - Stangen und Kolben bewegen sich mit
    - Slider eingebaut

--- Physolator/src/a07/LokomotiveTVG.java
+++ Physolator/src/a07/LokomotiveTVG.java
@@ -4,6 +4,8 @@ import static de.physolator.usr.tvg.Shape.*;
 import static java.lang.Math.*;
 import de.physolator.usr.tvg.TVG;
 import de.physolator.usr.util.Colors;
+import de.physolator.usr.util.parameter.Parameter;
+import de.physolator.usr.util.parameter.Slider;
 
 public class LokomotiveTVG extends TVG {
 	
@@ -18,62 +20,91 @@ public class LokomotiveTVG extends TVG {
 		scalesStyle.visible = true;
 	}
 	
+	@Parameter
+	@Slider(min = 0, max = 10, step=0.01, width = 200)
+	public double x = 0;
+	@Parameter
+	@Slider(min = 0, max = 10, step=0.01, width = 200)
+	public double y = 0;
+	@Parameter
+	@Slider(min = 0, max = 360, width = 200)
+	public int alpha = 0;
+	@Parameter
+	@Slider(min = 0, max = 360, width = 200)
+	public int beta = 0;
+	
 	public void paint() {
 		
+		//double t = lokomotive.getSimInfo().getActualFrameData().simulationTime;
 		style.useUCS = true;
 		style.strokeWidth = 3;
 		style.strokeColor = Colors.black;
 		style.fillColor = Colors.aquamarine4;//----> doesnt work
-		//chimneys
-		drawRectangle(2.8,2.6,3.2,3.1);
-		drawRectangle(3.8,2.6,4.2,3.1);
-		drawRectangle(5.8,2.6,6.8,4);
-		//corpus
-		drawRectangle(2.1,0.9,7.6,2.6);
-		//cabin
-		drawRectangle(0.5,0.9,2.1,3.4);
-		//steps
-		drawRectangle(0.25,0.6,1,0.9);
-		drawRectangle(0,0.3,0.7,0.6);
-		//roof
-		drawRectangle(0,3.4,2.6,3.7);
-		//cylinder
-		drawRectangle(4.8,0.5,6.7,0.9);
-		//wheels
-		style.strokeWidth = 2;
-		drawCircle(2,0.7,0.7);
-		drawaxis(2,0.7,0.7);
-		drawCircle(3.7,0.7,0.7);
-		drawaxis(3.7,0.7,0.7);
-		drawCircle(7.5,0.4,0.45);
-		drawaxis(7.5,0.4,0.45);
-		//window
-		style.strokeWidth = 3;
-		drawRectangle(0.8,2.2,1.8,3.2);
-		//circles
-		double angle = ((360/10)*5)+90;
-		double radians = angle * (Math.PI / 180);
-		drawCircle(2-cos(radians)*0.5,0.7-sin(radians)*0.5, 0.15);
-		drawCircle(3.7-cos(radians)*0.5,0.7-sin(radians)*0.5, 0.15);
-		//square
-		drawRectangle(5.4,0.55,5.7,0.9);
-		//connectionLines
-		drawLine(2-cos(radians)*0.5,0.7-sin(radians)*0.5,3.7-cos(radians)*0.5,0.7-sin(radians)*0.5);
-		drawLine(3.7-cos(radians)*0.5,0.7-sin(radians)*0.5,5.4+((5.7-5.4)/2),0.55+((0.9-0.55)/2));
+		
+		zeichneLokomotive(x,y,alpha,beta);
+		
 	}
 	
-	public void drawaxis(double x1, double y1, double r )
+	/*public void drawchimneys(double x1, double y1, double x2, double y2)
+	{
+		drawRectangle(x1,y1,3.2,y2);
+		drawRectangle(x1,y1,4.2,y2);
+		drawRectangle(x1*2.07,y1,6.8,y2*1.29);	
+	}*/
+	
+	public void drawaxis(double x1, double y1,double gamma, double r )
 	{
 		for(double i = 0;i<=10;i++) {
 			
-			 double angle = ((360/10)*i)+90;
+			 double angle = ((360/10)*i)+gamma;
 			
-			double radians = angle * (Math.PI / 180);
+			double radians = -1*(angle * (Math.PI / 180));
 			
 		drawLine(x1,y1,x1-cos(radians)*r,y1-sin(radians)*r);
 		
 		}
 	}
+	
+	public void zeichneLokomotive(double x, double y, double alpha, double beta) 
+	{
+		
+				//chimneys
+				drawRectangle(2.8+x,2.6+y,3.2+x,3.1+y);
+				drawRectangle(3.8+x,2.6+y,4.2+x,3.1+y);
+				drawRectangle(5.8+x,2.6+y,6.8+x,4+y);
+				//corpus
+				drawRectangle(2.1+x,0.9+y,7.6+x,2.6+y);
+				//cabin
+				drawRectangle(0.5+x,0.9+y,2.1+x,3.4+y);
+				//steps
+				drawRectangle(0.25+x,0.6+y,1+x,0.9+y);
+				drawRectangle(0+x,0.3+y,0.7+x,0.6+y);
+				//roof
+				drawRectangle(0+x,3.4+y,2.6+x,3.7+y);
+				//cylinder
+				drawRectangle(4.8+x,0.5+y,6.7+x,0.9+y);
+				//window
+				drawRectangle(0.8+x,2.2+y,1.8+x,3.2+y);
+				//circles
+				double radians = -1*(alpha * (Math.PI / 180));
+				drawCircle((2-cos(radians)*0.5)+x,(0.7-sin(radians)*0.5)+y, 0.15);
+				drawCircle((3.7-cos(radians)*0.5)+x,(0.7-sin(radians)*0.5)+y, 0.15);
+				//square
+				drawRectangle(5.4-0.5*cos(radians)+x,0.55+y,5.7-0.5*cos(radians)+x,0.9+y);
+				//connectionLines
+				drawLine((2-cos(radians)*0.5)+x,(0.7-sin(radians)*0.5)+y,(3.7-cos(radians)*0.5)+x,(0.7-sin(radians)*0.5)+y);
+				
+				drawLine((3.7-cos(radians)*0.5)+x,(0.7-sin(radians)*0.5)+y,(5.4+((5.7-5.4)/2))-0.5*cos(radians)+x,(0.55+((0.9-0.55)/2)+y));
+				//wheels
+				style.strokeWidth = 2;
+				drawCircle(2+x,0.7+y,0.7);
+				drawaxis(2+x,0.7+y,alpha,0.7);
+				drawCircle(3.7+x,0.7+y,0.7);
+				drawaxis(3.7+x,0.7+y,alpha,0.7);
+				drawCircle(7.5+x,0.4+y,0.45);
+				drawaxis(7.5+x,0.4+y,beta,0.45);
+		
+		}
 	
 }
 
